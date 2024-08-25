24446fbb309948cd7910c6a61fe1276f1eb558ca
ChaseLewis <chaserlewis73003@gmail.com>
2024-08-25 18:34:05 -0500

    update build 1 more time so running `npm run build` automatically deploys everything.

--- app/vite.config.release.ts
+++ app/vite.config.release.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     base: '/PintaHelper/dist/',
     plugins: [react()],
+    build: {
+        outDir: "../dist",
+        rollupOptions: {
+            treeshake: "recommended"
+        }
+    }
 })
